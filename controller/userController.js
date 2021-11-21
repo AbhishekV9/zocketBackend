5ac656da6099a5d114f29e7e9fee6955f808b9ca
Abhsihek Verma <avishekv444@gmail.com>
2021-11-21 14:04:12 +0530

    respose according to showing toaster

--- controller/userController.js
+++ controller/userController.js
@@ -15,12 +15,18 @@ module.exports.RegisterUser= async function(req,res){
     let isValid=ValidateEmail(email);
     if(!isValid){
         return res.status(200).json({
-            message:"Email Entered Is Not Valid"
+            notValid:true,
+            registered:false,
+            isPresent:false,
+            message:"Please Enter A Valid Email"
         })
     }
     let user= await User.findOne({email:email});
     if(user){
         return res.status(200).json({
+            notValid:false,
+            registered:false,
+            isPresent:true,
             message:"User Already Registered",
             user
         })
@@ -28,10 +34,13 @@ module.exports.RegisterUser= async function(req,res){
 
     user=await User.create(req.body);
     return res.status(200).json({
+        notValid:false,
+        registered:true,
+        isPresent:false,
         message:"User Registered Successfully",
         user
     });
-    
+
    }catch(error){
       console.log(error);
       return res.json(500,{
